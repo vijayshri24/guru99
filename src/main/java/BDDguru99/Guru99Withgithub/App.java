55283bb40bcf68a79ec2d6d8f272f90faf1856b7
vijayshri24 <156435102+vijayshri24@users.noreply.github.com>
2024-01-19 17:10:09 +0530

    GURU99 creeated

---
+++ src/main/java/BDDguru99/Guru99Withgithub/App.java
@@ -0,0 +1,13 @@
+package BDDguru99.Guru99Withgithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
